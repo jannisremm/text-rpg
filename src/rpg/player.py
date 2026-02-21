7fe52e5c8279c1995ff3cc6c48ba7a5602468b2a
jannisremm <128300711+jannisremm@users.noreply.github.com>
2026-02-21 18:36:45 +0100

    feat: :sparkles: Initialise player class

--- src/rpg/player.py
+++ src/rpg/player.py
@@ -12,3 +12,15 @@
 #     add_item()
 
 # Hier lebt die Identität des Spielers.
+
+
+class Player:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age

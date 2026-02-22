163b80565545fc5b8e95a600de5fb74214c40ddf
jannisremm <128300711+jannisremm@users.noreply.github.com>
2026-02-23 00:29:43 +0100

    feat: :sparkles: Add inventory and hitpoints

--- src/rpg/player.py
+++ src/rpg/player.py
@@ -15,9 +15,16 @@
 
 
 class Player:
+    hitpoints = 100
+    inventory = ["Clothes", "Shoes", "Hat", "Gloves"]
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def __str__(self):
         return f"Hi I am {self.name} and I am {self.age} years old!"
+
+    def show_inventory(self):
+        string = " ".join(self.inventory)
+        return string

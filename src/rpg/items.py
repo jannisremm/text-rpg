2a0126466e0aeb78b5126fdbded310f3e80a272e
jannisremm <128300711+jannisremm@users.noreply.github.com>
2026-02-26 00:15:05 +0100

    feat: :art: Create base class Item which all items should inherit from

--- src/rpg/items.py
+++ src/rpg/items.py
@@ -10,11 +10,28 @@
 #     Anwendung auf Spieler, Gegner oder Welt
 
 
-class Potion:
-    def __init__(self, name, value, size):
+class Item:
+    """This class contains all basic properties of all items, which all items should inherit from"""
+
+    def __init__(self, name, weight=0, value=0, size="small", ai_description="No description yet"):
+
         self.name = name
+        self.weight = weight
         self.value = value
         self.size = size
+        self.ai_description = ai_description  # Placeholder for future idea:
+        # pass item properties to llm to generate unique description for each item
+        self.condition = 100  # as a percentage, where 100 is mint condition
+
+    def __str__(self) -> str:
+        item_description = f"It is a {self.name}, it looks {self.size} and {self.weight}, "
+        f"and has a value of {self.value}"
+        return item_description
+
+
+class Potion(Item):
+    def __init__(self, name, weight, value, size, description):
+        super().__init__(name, weight, value, size, description)
 
     def inspect(self):
         print(f"It looks like you can drink this {self.size} bottle of {self.name}")
@@ -24,9 +41,13 @@ class Potion:
 
 
 class HealingPotion(Potion):
-    def __init__(self, name, value, size, hitpoints):
-        super().__init__(name, value, size)
+    def __init__(self, name, weight, value, size, description, hitpoints):
+        super().__init__(name, weight, value, size, description)
         self.hitpoints = hitpoints
+
+    # def __init__(self, name, value, size, hitpoints):
+    #     super().__init__(name, value, size)
+    #     self.hitpoints = hitpoints
 
     def drink_potion(self):
         print(f"You drink the {self.size} healing potion, and feel better immmediately")
@@ -34,8 +55,8 @@ class HealingPotion(Potion):
 
 
 class Poison(Potion):
-    def __init__(self, name, value, size, damage):
-        super().__init__(name, value, size)
+    def __init__(self, name, weight, value, size, description, damage):
+        super().__init__(name, weight, value, size, description)
         self.damage = damage
 
     def drink_potion(self):
@@ -44,11 +65,11 @@ class Poison(Potion):
 
 
 if __name__ == "__main__":
-    red_potion = HealingPotion("Red potion", 5, "small", 12)
+    red_potion = HealingPotion("health potion", 1, 12, "small", "", 7)
 
     red_potion.inspect()
     red_potion.drink_potion()
 
-    green_potion = Poison("Cyanide", 8, "medium", 9)
+    green_potion = Poison("Cyanide", 1, 8, "medium", "", 9)
     green_potion.inspect()
     green_potion.drink_potion()
